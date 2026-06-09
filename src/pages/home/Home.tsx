0a99686c2c3844e460ec2515cd110b835e7ad74b
Carlos Cochero <cocheroc@gmail.com>
2026-06-09 01:41:10 -0500

    fix: corregir los 3 issues principales de React Doctor
    - Security: crear pnpm-workspace.yaml con minimumReleaseAge y trustPolicy
    - Performance: redondear decimales SVG en Icons.tsx a 2 dígitos (750 valores)
    - Accessibility: cambiar href='#' por href='/' en el logo del Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,7 @@ import Enviroment from "./components/enviroment/Enviroment";
 export default function Home() {
   return (
     <>
-    <a className="home" href="#"><img src="/Logo.svg" alt="Logo" className="logo" /></a>
+    <a className="home" href="/"><img src="/Logo.svg" alt="Logo" className="logo" /></a>
     <Enviroment />
     </>
 )};
