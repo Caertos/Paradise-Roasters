df26ce6fefa63643bc7071b3ab1f057437c09a1d
Carlos Cochero <cocheroc@gmail.com>
2026-06-15 21:58:18 -0500

    Agregados nuevos elementos de estilo de la pagina

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import Enviroment from "./components/enviroment/Enviroment";
 
+import "./home.css"
+
 export default function Home() {
   return (
-    <>
+    <div className="home">
     <a className="home" href="/"><img src="/Logo.svg" alt="Logo" className="logo" /></a>
     <Enviroment />
-    </>
+    </div>
 )};
--- src/pages/home/components/enviroment/Enviroment.tsx
+++ src/pages/home/components/enviroment/Enviroment.tsx
@@ -9,8 +9,8 @@ import "./enviroment.css";
 export default function Enviroment() {
   useScroll();
   return (
-    <section className="enviroment">
-      <div className="enviroment1">
+    <div className="enviroment">
+      <section className="enviroment1">
         <Sun />
         <Cloud source="nube1" />
         <Cloud source="nube2" />
@@ -19,18 +19,32 @@ export default function Enviroment() {
         <img src="/mountains.svg" alt="Mountains" className="mountains" />
         <div className="flamingos">
           <Flamingo source="flamingo" size="big" ext=".webp" />
-          <Flamingo source="flamingo" size="medium" ext=".webp" />
+          <Flamingo source="flamingo2" size="medium" ext=".webp" />
           <Flamingo source="flamingo" size="small" ext=".webp" />
-          <Flamingo source="flamingo" size="small" ext=".webp" />
-          <Flamingo source="flamingo" size="small" ext=".webp" />
+          <Flamingo source="flamingo3" size="small" ext=".webp" />
+          <Flamingo source="flamingo4" size="small" ext=".webp" />
         </div>
-        <div className="flamingos-silhouette">
+  <div className="flamingos-silhouette">
           <Flamingo source="flamingo-silhouette" size="medium" ext=".svg" />
-          <Flamingo source="flamingo-silhouette" size="medium" ext=".svg"  />
-          <Flamingo source="flamingo-silhouette" size="small" ext=".svg"  />
-          <Flamingo source="flamingo-silhouette" size="small" ext=".svg" />
+         <Flamingo source="flamingo-silhouette" size="small" ext=".svg"  />
+          <Flamingo source="flamingo-silhouette" size="x-small" ext=".svg"  />
+          <Flamingo source="flamingo-silhouette" size="x-small" ext=".svg" />
         </div>
-      </div>
+      </section>
+      <section className="enviroment2">
+        <img src="/mountains.svg" alt="Mountains" className="mountains-reflected"/>
+        <img src="/luzagua.webp" className="waterReflection"/>
+        <img src="/palmeras.webp" className="palm-trees"/>
+        <img src="/isla.webp" className="island"/>
+        <img src="/sombrasmarinas.webp" className="marine-shadow"/>
+      </section>
+    <section className="enviroment3">
     </section>
+    <section className="enviroment4">
+    </section>
+        <section className="enviroment4">
+    </section>    <section className="enviroment4">
+    </section>
+    </div>
   );
 }
